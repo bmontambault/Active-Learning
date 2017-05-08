085bfb83850b1f6e13a0c30301eb1b1a3fb44e7c
Brian Montambault <brianmontambault@gmail.com>
2017-05-08 02:38:22 +0000

    error in function list

--- flask_app.py
+++ flask_app.py
@@ -12,8 +12,8 @@ from config import nbars,max_score,trials
 goals=['max_score','find_max','min_error']
 functions=['pos_linear','neg_linear','pos_power','neg_power','pos_quad','neg_quad','sin','se']
 
-funcmap={f:''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10)) for f in functions.keys}
-revfuncmap={value:key for key,value in funcmap.iteritems()}       
+funcmap={f:''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10)) for f in functions}
+revfuncmap={value:key for key,value in funcmap.iteritems()}
 
 app=Flask(__name__)
 
@@ -27,14 +27,14 @@ def start():
             with open('data.json') as json_data:
                 data=json.load(json_data)
                 json_data.close()
-                
+
             if data['goal']==[]:
                 goal=goals[0]
                 function_name=functions[0]
             else:
                 prev_goal=goals.index(data['goal'][-1])
                 prev_func=functions.index(data['function_name'][-1])
-        
+
                 if len(functions)-prev_func==1:
                     if len(goals)-prev_goal==1:
                         goal=goals[0]
@@ -53,7 +53,7 @@ def start():
             else:
                 function_name=revfuncmap[function_tag]
             return task('test',goal,function_name,index)
-            
+
 
 @app.route('/admin',methods=['GET','POST'])
 def start_admin():
@@ -68,13 +68,13 @@ def start_admin():
             function_name=revfuncmap[function_tag]
         index=request.form['index']
         return task('test',goal,function_name,index)
-        
+
 def task(idtag,goal,function_name,index):
     with open('functions.json') as json_data:
             function=json.load(json_data)[function_name]
             json_data.close()
 
-    if index=='exit':            
+    if index=='exit':
         with open('data.json') as json_data:
             data=json.load(json_data)
             json_data.close()
@@ -92,7 +92,7 @@ def task(idtag,goal,function_name,index):
         with open('data.json', 'w') as json_data:
             json.dump(data,json_data)
         return 'Thank you for participating. Your final score is {0}'.format(data['final_score'][-1].split('.')[0])
-    
+
     elif goal=='max_score':
         if index=='0':
             return render_template('max_score_instructions.html',trials=trials,goal=goal,function_name=funcmap[function_name])
@@ -102,7 +102,7 @@ def task(idtag,goal,function_name,index):
             test_response=request.form['test_response']
             final_score=request.form['final_score']
             return render_template('exit_survey.html',goal=goal,function_name=funcmap[function_name],test_response=test_response,final_score=final_score)
-    
+
     elif goal=='find_max':
         if index=='0':
             return render_template('find_max_instructions.html',goal=goal,function_name=funcmap[function_name])
@@ -125,6 +125,6 @@ def task(idtag,goal,function_name,index):
             test_response=request.form['test_response']
             final_score=request.form['final_score']
             return render_template('exit_survey.html',goal=goal,function_name=funcmap[function_name],test_response=test_response,final_score=final_score)
-            
+
 if __name__=="__main__":
     app.run()
