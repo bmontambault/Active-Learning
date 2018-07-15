1ac4cc5b99a99cffc2b89caa53784710a47b85dd
bmontambault <brianmontambault@gmail.com>
2018-07-14 23:42:07 -0400

    add max score

--- models/visualize_responses.py
+++ models/visualize_responses.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
 import json
+import numpy as np
 
 
 app=Flask(__name__)
 app.secret_key=''
 app.config['SESSION_TYPE']='filesystem'
 
-with open('test_model_results.json', 'r') as f:
+with open('all_model_results.json', 'r') as f:
     model_results = json.load(f)
     
 
@@ -16,7 +17,8 @@ def index():
     ids = [model_results[i]['id'] for i in range(len(model_results))]
     functions = [model_results[i]['function_name'] for i in range(len(model_results))]
     goals = [model_results[i]['goal'] for i in range(len(model_results))]
-    scores = [model_results[i]['score'] for i in range(len(model_results))]
+    scores = [np.round(model_results[i]['score'], 2) for i in range(len(model_results))]
+    max_scores = [np.round(model_results[i]['max_score'], 2) for i in range(len(model_results))]
     
     for d in model_results:
         d['models'] = [m for m in d['models'] if m['acquisition'] != 'Phase']
@@ -29,8 +31,8 @@ def index():
         else:
             model_name.append(m['acquisition'])
     pseudo_r2 = [m['pseudo_r2'] for m in best_model]
-    params = [m['acquisition_params'] + m['decision_params'] for m in best_model]
-    return render_template('index.html', ids = ids, functions = functions, goals = goals, scores = scores, model_name = model_name, params = params, pseudo_r2 = pseudo_r2)
+    params = [np.round(m['acquisition_params'] + m['decision_params'], 2).tolist() for m in best_model]
+    return render_template('index.html', ids = ids, functions = functions, goals = goals, scores = scores, max_scores = max_scores, model_name = model_name, params = params, pseudo_r2 = pseudo_r2)
 
 
 @app.route('/<participant_id>')
