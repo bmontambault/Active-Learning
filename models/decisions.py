7d5dec470d70d71b1e1578dde56c5bb39defc271
bmontambault <brianmontambault@gmail.com>
2018-07-30 21:58:39 -0400

    added softmax functions

--- models/decisions.py
+++ models/decisions.py
@@ -26,6 +26,25 @@ class Softmax:
         return exp_u / np.nansum(exp_u)
     
     
+class PhaseSoftmax:
+    
+    init_params = [1., 12]
+    bounds = [(.001, 10), (.1, 24)]
+    
+    def __init__(self, choices, best_action, trial):
+        self.choices = choices
+        self.best_action = best_action
+        self.trial = trial
+        
+    def __call__(self, utility, temperature, phase):
+        if self.trial + 1 > phase:
+            utility = np.array([np.exp(-abs(self.best_action - x)) for x in self.choices])
+            
+        centered_utility = utility - np.nanmax(utility)
+        exp_u = np.exp(centered_utility / temperature)
+        return exp_u / np.nansum(exp_u)
+    
+    
 class RandomStaySoftmax:
     
     init_params = [1., .5]
@@ -40,26 +59,3 @@ class RandomStaySoftmax:
         u = exp_u / np.nansum(exp_u)
         u = np.array([ui + stay if ui == self.action_1 else ui for ui in u])
         return u / np.sum(u)
-    
-    
-class PhaseSoftmax:
-    
-    init_params = [1., 12]
-    bounds = [(.001, 10), (.1, 24)]
-    
-    def __init__(self, choices, best_action, mean, trial):
-        self.choices = choices
-        self.best_action = best_action
-        self.mean = mean
-        self.trial = trial
-        
-    def __call__(self, utility, temperature, phase):
-        if self.trial > phase:
-            if np.all(self.mean != None):
-                utility = self.mean / np.sum(self.mean)
-            else:
-                utility = np.array([np.exp(-abs(self.best_action - x)) for x in self.choices])
-            
-        centered_utility = utility - np.nanmax(utility)
-        exp_u = np.exp(centered_utility / temperature)
-        return exp_u / np.nansum(exp_u)
